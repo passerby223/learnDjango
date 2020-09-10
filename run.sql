add1026fc9633894beeba8519dabd8a0c5c02e06
passerby223 <1912315910@qq.com>
2020-09-11 00:30:10 +0800

    add sql文件

---
+++ run.sql
@@ -0,0 +1,10 @@
+-- 查询数据-基本查询
+-- 查询出students表的所有数据
+# select * from students;
+-- 查询出classes表的所有数据
+# SELECT * FROM classes;
+-- 下述查询会直接计算出表达式的结果。虽然SELECT可以用作计算，但它并不是SQL的强项。
+-- 但是，不带FROM子句的SELECT语句有一个有用的用途，就是用来判断当前到数据库的连接是否有效。
+-- 许多检测工具会执行一条SELECT 1;来测试数据库连接。
+# SELECT 100+200;
+# select 1;
